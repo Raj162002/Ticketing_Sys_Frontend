95b880a3e60cc96b1b1d0b9840635cb4c5b142f9
Raj162002 <Ramkumarrajeevan16@gmail.com>
2024-12-02 12:41:27 +0530

    Created Basic html for configuration form

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import ConfigurationForm from './components/ConfigurationForm'
 
 function App() {
-  const [count,setCount]= useState(4);
 
-  function decrementFunction(){
-    setCount(count => count-1);
-  }
-  
-  function incrementFunction(){
-    setCount(count => count+1);
-  }
 
   return (
     <>
-    <button onClick={decrementFunction}>-</button>
-    <span>{count}</span>
-    <button onClick={incrementFunction}>+</button>
-     
+     <ConfigurationForm/>
     </>
   )
 }
--- src/components/ConfigurationForm.tsx
+++ src/components/ConfigurationForm.tsx
@@ -1,4 +1,29 @@
 function ConfigurationForm(){
+    return(
+        <form>
+            <h2>Configuration Form</h2>
+            <div>
+                <label htmlFor="mticketc">Maximum Ticket Count per Vendor: </label>
+                <input type="number" id="mticketc" required />
+            </div>
+            <div>
+                <label htmlFor="eventN">Enter the Event Name: </label>
+                <input type="text" id="eventN" required />
+            </div>
+            <div>
+                <label htmlFor="vendorNo">Enter the number of vendors: </label>
+                <input type="number" id="vendorNo" required />
+            </div>
+            <div>
+                <label htmlFor="ticketRetrival">Enter the ticket retrival rate per second: </label>
+                <input type="number" id="ticketRetrival" required />
+            </div>
+            <div>
+                <label htmlFor="customerRetrival">Enter the customer retrival rate per second: </label>
+                <input type="number" id="customerRetrival" required />
+            </div>
+        </form>
+    )
 
 }
 export default ConfigurationForm;
